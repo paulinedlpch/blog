9b4d757e8cce356ba2ffc6008a89621c0d81c5a1
paulinedlpch <pauline.delpeuch@gmail.com>
2025-04-11 19:53:56 +0000

    created next app

---
+++ blog/next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+// import type { Configuration } from 'webpack-dev-middleware';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  // webpackDevMiddleware: (config: Configuration) => {
+  //   config.watchOptions = {
+  //     poll: 1000,
+  //     aggregateTimeout: 300,
+  //   }
+  //   return config
+  // },
+};
+
+export default nextConfig;
